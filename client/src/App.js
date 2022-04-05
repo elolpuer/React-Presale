f3dbc567bc0f0d960dde224efd25bacfacfe0578
elolpuer <egorgoncharov6755@gmail.com>
2022-04-05 12:25:19 +0300

    sign

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PresaleDBTContract from "./contracts/PresaleDBT.sol/PresaleDBT.json";
 import PresaleDIGContract from "./contracts/PresaleDIG.sol/PresaleDIG.json";
-import Signs from "./signatures/sing.json"
+import Signs from "./signatures/signs.json"
 import getWeb3 from "./getWeb3";
 import "./App.css";
 import {
@@ -24,7 +24,8 @@ class App extends Component {
     contractDIG: null,
     amount: 0,
     priceDBT: "0.00007",
-    priceDIG:"0.07"
+    priceDIG:"0.07",
+    signDBT: {}
   };
 
   // createMarketItem = this.createMarketItem.bind(this)
@@ -43,7 +44,7 @@ class App extends Component {
       const instanceDIG = new web3.eth.Contract(
         PresaleDIGContract.abi
       );
-      instanceDBT.options.address = "0x1613beB3B2C4f22Ee086B2b38C1476A3cE7f78E8"
+      instanceDBT.options.address = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
       instanceDIG.options.address = "0x9E545E3C0baAB3E08CdfD552C960A1050f373042"
       // function init() {
         // Tabletop.init({
@@ -69,17 +70,20 @@ class App extends Component {
         account: accounts[0]
       })
       let flag = false;
+      let newSign = {}
       Signs.map(
         (value) => {
           if(value.address === accounts[0]) {
             flag = true
+            newSign.v = value.v
+            newSign.r = value.r
+            newSign.s = value.s
           }
         }
       )
       if (flag === false) {
         throw "error"
       }
-
       // const accounts = await window.ethereum.request({
       //   method: "eth_requestAccounts"
       // });
@@ -87,7 +91,8 @@ class App extends Component {
       this.setState({
         web3,
         contractDBT: instanceDBT,
-        contractDIG: instanceDIG
+        contractDIG: instanceDIG,
+        signDBT: newSign
       }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -114,13 +119,13 @@ class App extends Component {
     // this.setState({
     //   account: accounts[0]
     // })
-    // const { account, contractDIG } = this.state;
+    const { account, contractDIG, contractDBT } = this.state;
     // await contractDIG.methods.mockRound1StartTime(
     //     (parseInt(startTime) - secondsInADay * 2).toString()
     // ).send({from: account})
-    // await contract.methods.mockRound1StartTime(
-    //         (parseInt(startTime) - secondsInADay * 2).toString()
-    //     ).send({from: account})
+    await contractDBT.methods.mockRound1StartTime(
+            (parseInt(startTime) - secondsInADay * 2).toString()
+        ).send({from: account})
   };
 
   buyDBT = this.buyDBT.bind(this)
@@ -141,12 +146,12 @@ class App extends Component {
   }
 
   async buyDBT(amount) {
-    const {account, contractDBT, priceDBT, web3} = this.state
+    const {account, contractDBT, priceDBT, web3, signDBT} = this.state
     const priceSend = web3.utils.toWei(priceDBT)
     const value = parseInt(amount) * parseInt(priceSend)
-    const r = '0xdaba3f744ada07faadb185875b74cc9077ed383d7dc962214930f2aa70ae331e'
-    const s = '0x31fc53e4da5afcbf01a0431cdbdfea46b334ec4037dffcf7f3cf2ef9c39da79d'
-    const v = 28
+    const r = signDBT.r
+    const s = signDBT.s
+    const v = signDBT.v
     await contractDBT.methods.buyDBTRound1(
       v,
       r,
@@ -193,7 +198,7 @@ class App extends Component {
 
   //dig 0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB
   async addTokenToMetamask() {
-      const tokenAddress = '0xa82fF9aFd8f496c3d6ac40E2a0F282E47488CFc9';
+      const tokenAddress = '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9'
       const tokenSymbol = 'DBT';
       const tokenDecimals = 18;
 
